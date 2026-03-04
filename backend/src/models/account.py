f048e8a21e8fd4f9a086b7f76932fbb9c94d212a
GABRIEL-SANCHES-V <79290891+GABRIEL-SANCHES-V@users.noreply.github.com>
2026-03-04 14:31:34 -0300

    Model Account Pronto

---
+++ backend/src/models/account.py
@@ -0,0 +1,80 @@
+from decimal import Decimal
+from .client import Client
+from .transaction import Transaction
+
+
+class Account():
+    """
+        Represents a bank account.
+
+        Attributes:
+            cliente (Client): The client who owns the account.
+            account_number (int): The account number.
+            withdraws_today (int): The number of withdrawals made today.
+            balance (Decimal): The current balance of the account.
+            extract (list): A list of transactions made on the account.
+            password (str): The password for account access.
+    """
+    def __init__(self, cliente: Client, account_number: int,  password: str, balance: Decimal = Decimal("0.00")):
+        self._cliente = cliente
+        self._account_number = account_number
+        self._withdraws_today = 0
+        self._balance = balance
+        self._extract = []
+        self._password = password
+
+    def __str__(self):
+        title = "Dados da Conta".center(50, "-")
+        footer = "".center(50, "-")
+
+        return (
+            f"\n{self.cliente}\n"
+            f"{title}\n"
+            f"Número da Conta: {self.account_number}\n"
+            f"Saldo: R$ {self.balance:.2f}\n"
+            f"{self.format_extract()}\n"
+            f"{footer}"
+        )
+    
+    @property
+    def cliente(self):
+        return self._cliente
+
+    @property
+    def account_number(self):
+        return self._account_number
+    
+    @property
+    def balance(self):
+        return self._balance
+    
+    def _increment_balance(self, value: Decimal):
+        self._balance += value
+
+    def _decrement_balance(self, value: Decimal):
+        self._balance -= value
+
+    @property
+    def withdraws_today(self):
+        return self._withdraws_today
+    
+    def increment_withdraws_today(self):
+        self._withdraws_today += 1
+
+    def reset_withdraws_today(self):
+        self._withdraws_today = 0
+
+    @property
+    def extract(self):
+        return list(self._extract)
+    
+    def add_transaction_to_extract(self, transaction: Transaction):
+        self._extract.append(transaction)
+    
+    @property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, password: str):
+        self._password = password
