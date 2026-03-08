6441282d490c676b4e63ca58574126ad9478c707
GABRIEL-SANCHES-V <79290891+GABRIEL-SANCHES-V@users.noreply.github.com>
2026-03-08 18:03:31 -0300

    Adiciona PhoneNumber à lista de Value Objects exportados

--- backend/src/domains/value_objects/__init__.py
+++ backend/src/domains/value_objects/__init__.py
@@ -1,7 +1,9 @@
 from domains.value_objects.email import Email
 from domains.value_objects.cpf import CPF
+from domains.value_objects.phone_number import PhoneNumber
 
 __all__ = [
     "Email",
-    "CPF"
+    "CPF",
+    "PhoneNumber",
 ]
